1ee1b74c5661e3970d8257898baa97fa35fe84ae
k-gilvary <kennygilvary82@gmail.com>
2024-10-16 15:18:16 -0400

    model validations and constraints, sequelize error handles

--- db/models/user.js
+++ db/models/user.js
@@ -11,23 +11,74 @@ module.exports = sequelize.define('user', {
       type: DataTypes.INTEGER
     },
     userType: {
-      type: DataTypes.STRING
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+            msg: 'userType cannot be null'
+        },
+        notEmpty: {
+            msg: 'userType cannot be empty'
+        }
+      }
     },
     firstName: {
-      type: DataTypes.STRING
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+            msg: 'firstName cannot be null'
+        },
+        notEmpty: {
+            msg: 'firstName cannot be empty'
+        }
+      }
     },
     lastName: {
-      type: DataTypes.STRING
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+            msg: 'lastName cannot be null'
+        },
+        notEmpty: {
+            msg: 'lastName cannot be empty'
+        }
+      }
     },
     email: {
-      type: DataTypes.STRING
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+            msg: 'email cannot be null'
+        },
+        notEmpty: {
+            msg: 'email cannot be empty'
+        },
+        isEmail: {
+            msg: 'Invalid email id'
+        }
+      }
     },
     password: {
-      type: DataTypes.STRING
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+            msg: 'password cannot be null'
+        },
+        notEmpty: {
+            msg: 'password cannot be empty'
+        }
+      }
     },
     confirmPassword: {
         type: DataTypes.VIRTUAL,
         set(value) {
+            if (this.password.length < 8) {
+                throw new AppError('Password must have at least 8 characters', 400);
+            }
             if (value === this.password) {
                 const hashPassword = bcrypt.hashSync(value, 10);
                 this.setDataValue('password', hashPassword);
