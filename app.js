45092e5345353c56327271667607bca122193838
k-gilvary <kennygilvary82@gmail.com>
2024-10-16 00:02:58 -0400

    set up express server

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.status(200).json({
+        status: 'success',
+        message: 'Hello'
+    });
+});
+
+app.listen(3000, () => {
+    console.log('server up', 3000);
+});
